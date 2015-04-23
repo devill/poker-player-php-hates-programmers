c8cad196d718a4fd6d34b9b1892965518a1dd066
devill <devillmeister@gmail.com>
2015-04-23 13:39:29 +0200

    haha

--- player.php
+++ player.php
@@ -2,11 +2,11 @@
 
 class Player
 {
-    const VERSION = "Default PHP folding player";
+    const VERSION = "All lean";
 
     public function betRequest($game_state)
     {
-        return 0;
+        return 1000;
     }
 
     public function showdown($game_state)
